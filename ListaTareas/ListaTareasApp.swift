99e8bc8f1fb4f5d98e44072ae6ce0939b402b93c
Diego Herreros <diegohp85@gmail.com>
2025-01-20 15:19:24 +0100

    Initial Commit

---
+++ ListaTareas/ListaTareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaTareasApp.swift
+//  ListaTareas
+//
+//  Created by Diego Herreros Parron on 20/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ListaTareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
